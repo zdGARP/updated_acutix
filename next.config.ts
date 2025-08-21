6abdf98eccdc0e6d37239f5f1fe239c810b09782
Aloseous Kingsly <50615352+Aloseous@users.noreply.github.com>
2025-08-21 14:21:15 +0530

    image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true // disables Vercel's Image Optimization
+  }
 };
 
 export default nextConfig;
